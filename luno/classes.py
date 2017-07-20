62963deb0688d22c33217d72ff68f3509f1d2731
wilhelmg <wilhelm.guldenpfennig@sunrise.co.za>
2017-07-20 20:01:48 +0200

    -still figuring out subscription, now trying threading

--- luno/classes.py
+++ luno/classes.py
@@ -1,8 +1,11 @@
 import os
 import logging
+from threading import Thread
 import inspect
 import json
 import time
+import sys
+import hashlib
 
 from pprint import pprint, pformat
 
@@ -13,9 +16,10 @@ from websocket import create_connection, WebSocketTimeoutException
 logging.basicConfig(level=logging.WARNING)
 log = logging.getLogger(__name__)
 
+
 # fh = logging.FileHandler('LunoStream.log')
 # fh.setLevel(logging.DEBUG)
-# log.setLevel(logging.DEBUG)
+# log.setLevel(logging.INFO)
 
 
 class Stream(object):
@@ -35,12 +39,12 @@ class Stream(object):
 
         self.addr = None
         self.conn = None
-        # self.channels = {}
+        self.channels = {}
 
         if not key or not secret and not creds:
             self._handle_creds(path_to_creds)
 
-        # self._handle_configs()
+        self._handle_configs()
 
     def _handle_creds(self, path_to_creds):
         if not path_to_creds:
@@ -71,11 +75,15 @@ class Stream(object):
         self.conn.close()
         self.conn = None
 
-    # def _handle_configs(self):
-    #     from yaml import load
-    #     with open("config.yaml", 'r') as read:
-    #         configs = load(read)
-    #     self.channels = configs['channels']
+    def _handle_configs(self):
+        import definitions
+        from yaml import load
+
+        config_path = os.path.join(definitions.MODULE_ROOT, 'config.yaml')
+        with open(config_path, 'r') as read:
+            configs = load(read)
+
+        self.channels = configs['channels']
 
     def _connect(self):
         function_name = inspect.stack()[0][3]
@@ -160,6 +168,8 @@ class Stream(object):
         log.debug("\nOrder {} added to {}s.\n".format(order['create_update']['order_id'],
                                                       order['create_update']['type'].lower()))
 
+        self._handle_subscriptions("order_book")
+
         return True
 
     def _remove_order(self, order_id):
@@ -168,17 +178,21 @@ class Stream(object):
 
         order, index, location = self.fetch_order(order_id)
 
-        # if not order:
-
-
         try:
             eval("self.{}.pop({})".format(location, index))
 
         except IndexError:
             log.warning("\nOrder '{}' not found in self.{}.\n".format(order_id, location))
             return False
+
+        self._handle_subscriptions("order_book")
+
         log.debug("\nOrder {} removed from {}.\n".format(order_id, location))
         return True
+
+    def __trade_hash(self, trade):
+        trade_string = "".join(sorted([str(i) for i in trade.values()])).encode('utf-8')
+        return hashlib.md5(trade_string).hexdigest()
 
     def _add_trade(self, trade, timestamp):
         function_name = inspect.stack()[0][3]
@@ -195,44 +209,73 @@ class Stream(object):
                      "counter": trade["counter"],
                      "timestamp": timestamp}
 
+        trade_hash = self.__trade_hash(new_trade)
+
+        new_trade['hash'] = trade_hash
+
         self._trade_history.insert(0, new_trade)
 
         self._trades = new_trade
         self._ticker = new_trade['price']
 
+        self._handle_subscriptions("trade")
+
         log.info("\nNew trade added: \n{}\n".format(pformat(new_trade)))
         return True
 
-    # def _handle_subscriptions(self, order_type):
-    #     if order_type == "trade":
-    #         if self.channels["trades"] and self.channels["ticker"]:
-    #             return [self._trade_history[0], round(self._trade_history[0]['price'], 0)]
-    #
-    #         elif self.channels["trades"]:
-    #             return self._trade_history[0]
-    #
-    #         elif self.channels["ticker"]:
-    #             return round(self._trade_history[0]['price'], 0)
-    #
-    #     elif self.channels["order_book"]:
-    #         return {"bids": self.bids,
-    #                 "asks": self.asks}
-    #
-    # def subscribe(self, channel):
-    #     self.channels[channel] = True
-    #
-    # def get_available_channels(self):
-    #     return [i for i in self.channels.keys()]
+    def _handle_subscriptions(self, order_type):
+        if order_type == "trade":
+            if self.channels["ticker"]:
+                if self._trade_history:
+                    payload = json.dumps(round(self._trade_history[0]['price'], 0))
+                    print(payload)
+
+            if self.channels["trades"]:
+                if self._trades:
+                    payload = json.dumps(self._trades)
+                    print(payload)
+
+            if self.channels["trade_history"]:
+                if self._trade_history:
+                    payload = json.dumps(self._trade_history)
+                    print(payload)
+
+        elif self.channels["order_book"]:
+            if self._order_book:
+                payload = json.dumps(self._order_book)
+                print(payload)
+
+    def _subscription_thread(self):
+        self.manager()
+
+    def subscribe(self, channel):
+        self.channels[channel] = True
+        sub_thread = Thread(target=self._subscription_thread)
+        sub_thread.start()
+
+    def get_available_channels(self):
+        channels = [i for i in self.channels.keys()]
+        print("Available channels: ")
+        for channel in channels:
+            print(channel)
+        print("\n")
+
+        return channels
+
     def ticker(self):
+        self.manager()
         return self._ticker
 
     def trades(self):
+        self.manager()
         return self._trades
 
     def order_book(self):
+        self.manager()
         return self._order_book
 
     def trade_history(self):
+        self.manager()
         return self._trade_history
 
     def fetch_order(self, order_id):
@@ -257,24 +300,24 @@ class Stream(object):
         return order, index, location
 
     def receive(self):
-        data = self.conn.recv()
-        if data:
-            return json.loads(data)
-        return
+        return json.loads(self.conn.recv())
 
-    def manager(self, order):
+    def manager(self):
         function_name = inspect.stack()[0][3]
         log.debug(function_name)  # Log function name upon execution
 
         log.debug("\nProcessing new order...\n")
 
         # order_type = None
+        order = self.receive()
+        if not order:
+            return
 
         if "asks" in order:
             self.bids = order['bids']
             self.asks = order['asks']
             self._order_book = {"bids": self.bids,
-                               "asks": self.asks}
+                                "asks": self.asks}
             # order_type = "order_book"
         # log.info("\nFormatting bids and asks...\n")
         # self.__format_bids(order['bids'])
@@ -293,7 +336,7 @@ class Stream(object):
             elif order['trade_updates']:
                 for trade in order["trade_updates"]:
                     self._add_trade(trade, order['timestamp'])
-                # order_type = "trade"
+                    # order_type = "trade"
 
         # if order_type:
         #     return self._handle_subscriptions(order_type)
